2f10299eed3e264e7b12bb68f5ce2c133dabac76
duckies <7842848+duckies@users.noreply.github.com>
2024-12-17 13:51:48 -0500

    chore: disable incomplete WarcraftLogs command

--- apps/bot/src/client.ts
+++ apps/bot/src/client.ts
@@ -1,15 +1,21 @@
 import { Client } from "@repo/disco";
 import { Partials } from "discord.js";
 import { PingCommand } from "./commands/ping";
-import { WarcraftLogsCommand } from "./commands/warcraftlogs/wcl";
+// import { WarcraftLogsCommand } from "./commands/warcraftlogs/wcl";
 import { onMessageDelete } from "./events/message-remove";
 import { onMemberRemove } from "./events/member-remove";
 
 export const client = new Client({
-  intents: ["Guilds", "GuildMessages", "MessageContent", "GuildVoiceStates", "GuildMembers"],
+  intents: [
+    "Guilds",
+    "GuildMessages",
+    "MessageContent",
+    "GuildVoiceStates",
+    "GuildMembers",
+  ],
   partials: [Partials.Message, Partials.GuildMember],
   commander: {
-    commands: [PingCommand, WarcraftLogsCommand],
+    commands: [PingCommand],
   },
-  listeners: [onMessageDelete, onMemberRemove]
+  listeners: [onMessageDelete, onMemberRemove],
 });
